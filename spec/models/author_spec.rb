b918faa910089ffeb54e4d43a07cfed4602226c4
Christian Flach <cmfcmf.flach@gmail.com>
2016-10-31 17:01:01 +0100

    Add name method to authors, closes #7.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   context "with Alan Turing as author" do
     it "the name method returns his full name" do
-      author = Author.create!(:first_name => "Alan", :last_name => "Turing")
+      author = create(:author)
+
       expect(author.name).to eq("Alan Turing")
     end
   end
