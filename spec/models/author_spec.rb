258cbd6ba567ac4b9dc15d681ca6ebbc8fe18ff6
Christian Flach <cmfcmf.flach@gmail.com>
2016-10-31 17:41:04 +0100

    Failing test for #13.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -8,4 +8,27 @@ RSpec.describe Author, type: :model do
       expect(author.name).to eq("Alan Turing")
     end
   end
+  context "with no authors" do
+    it "requires first and last name" do
+      author = Author.new
+      expect {
+        author.save!
+      }.to raise_error(ActiveRecord::RecordInvalid)
+
+      author = Author.new(:first_name => "Alan")
+      expect {
+        author.save!
+      }.to raise_error(ActiveRecord::RecordInvalid)
+
+      author = Author.new(:last_name => "Turing")
+      expect {
+        author.save!
+      }.to raise_error(ActiveRecord::RecordInvalid)
+
+      author = Author.new(:first_name => "Alan", :last_name => "Turing")
+      expect {
+        author.save!
+      }.to_not raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
