ec9df0f0624ea7367f1a4821dd9e7fb37b15d1d0
Christian Flach <cmfcmf.flach@gmail.com>
2016-10-31 16:49:25 +0100

    Save new authors, closes #6.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,20 @@ class AuthorsController < ApplicationController
   def new
 
   end
+
+  def create
+    @author = Author.new(author_params)
+
+    @author.save
+    redirect_to @author
+  end
+
+  def show
+    render plain: ''
+  end
+
+  private
+    def author_params
+      params.require(:author).permit(:first_name, :last_name, :homepage)
+    end
 end
