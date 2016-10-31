797161b60d2c9d05d35cc2827a069752dd8fd8ef
Christian Flach <cmfcmf.flach@gmail.com>
2016-10-31 16:30:05 +0100

    Fix casing, closes #6.

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -24,6 +24,6 @@ describe "Author creation page", :type => :feature do
 
   it "should have a submit button" do
     visit '/authors/new'
-    expect(page.find_button("Create author").value).to eq("Create author")
+    expect(page.find_button("Create Author").value).to eq("Create Author")
   end
 end
