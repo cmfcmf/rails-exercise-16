432e8451fb0188c88864aef33411cb46ff8bd1d1
Christian Flach <cmfcmf.flach@gmail.com>
2016-10-31 16:14:29 +0100

    Failing test for #3.

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -6,4 +6,19 @@ describe "Author creation page", :type => :feature do
     visit '/authors/new'
     expect(page).to have_text("Create author")
   end
+
+  it "should have a 'first name' field" do
+    visit '/authors/new'
+    expect(page).to have_text("First name")
+  end
+
+  it "should have a 'last name' field" do
+    visit '/authors/new'
+    expect(page).to have_text("Last name")
+  end
+
+  it "should have a 'homepage' field" do
+    visit '/authors/new'
+    expect(page).to have_text("Homepage")
+  end
 end
