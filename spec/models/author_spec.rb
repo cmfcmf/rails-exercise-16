0077021d80ff341096c2693f496531f9f1d60522
Christian Flach <cmfcmf.flach@gmail.com>
2016-10-31 16:54:15 +0100

    Add failing test for #7.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "with Alan Turing as author" do
+    it "the name method returns his full name" do
+      author = Author.create!(:first_name => "Alan", :last_name => "Turing")
+      expect(author.name).to eq("Alan Turing")
+    end
+  end
 end
